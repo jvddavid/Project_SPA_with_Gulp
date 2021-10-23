82d07e351db8b6390ad660df3db429013d3b1ed8
jvddavid <j.victordavid2@gmail.com>
2021-10-23 12:51:35 -0300

    install the packges

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/*
+Author: João Victor David de Oliveira (j.victordavid2@gmail.com)
+gulpfile.js (c) 2021
+Desc: gulp file for SPA project
+Created:  2021-10-23T15:41:43.914Z
+Modified: 2021-10-23T15:41:57.117Z
+*/
+
