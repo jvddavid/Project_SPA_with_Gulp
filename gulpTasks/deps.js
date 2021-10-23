9fbe96d6606edbc26908cb3adda57fd86a9a66e7
jvddavid <j.victordavid2@gmail.com>
2021-10-23 17:44:49 -0300

    finish create the tasks of gulp

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -3,19 +3,25 @@ Author: João Victor David de Oliveira (j.victordavid2@gmail.com)
 deps.js (c) 2021
 Desc: Tasks relacionadas as dependencias
 Created:  2021-10-23T19:40:16.577Z
-Modified: 2021-10-23T19:45:08.334Z
+Modified: 2021-10-23T20:22:11.062Z
 */
 
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 
-function depsCSS(cb) {
-    return cb()
+function depsCSS() {
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+        .pipe(uglifycss({ "uglyComments": false }))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
 
-function depsFonts(cb) {
-    return cb()
+function depsFonts() {
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = {
--- gulpTasks/servidor.js
+++ gulpTasks/servidor.js
@@ -3,18 +3,37 @@ Author: João Victor David de Oliveira (j.victordavid2@gmail.com)
 servidor.js (c) 2021
 Desc: Controlar o servidor de desenvolvimento
 Created:  2021-10-23T19:41:14.815Z
-Modified: 2021-10-23T19:47:58.724Z
+Modified: 2021-10-23T20:39:35.437Z
 */
 
 
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
 function monitorarArquivos(cb) {
+    watch('src/**/*.html', () => {
+        return gulp.series('appHTML')()
+    })
+    watch('src/assets/sass/**/*.sass', () => {
+        return gulp.series('appCSS')()
+    })
+    watch('src/assets/js/**/*.js', () => {
+        return gulp.series('appJS')()
+    })
+    watch('src/assets/imgs/**/*.*', () => {
+        return gulp.series('appIMG')()
+    })
+
     return cb()
 }
 
-function servidor(cb) {
-    return cb()
+function servidor() {
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 8080,
+            livereload: true
+        }))
 }
 
 
